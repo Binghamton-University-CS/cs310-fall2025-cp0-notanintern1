5a3eb02191fa35f5b5a5800893f183c5269e77f6
asafford <asafford@cs.binghamton.edu>
2025-08-26 18:43:54 -0400

    assignment complete

--- zoo.cpp
+++ zoo.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 #include <stdlib.h>
+#include "Animal.h"
+#include "AnimalsInZoo.h"
+
 using namespace std;
-
-class Animal {
-   public:
-      Animal(string speciesName, unsigned int discoveryYear) {
-         species = speciesName;
-         year_discovered = discoveryYear;
-      }
-
-      Animal() : species(""), year_discovered(0) {};
-
-      void display() {
-         cout << species << " [" << year_discovered << "]" << endl;
-      }
-
-   private:
-      string species = "";
-      unsigned int year_discovered = 0;
-};
 
 int main() {
    Animal *animal1 = new Animal("African Elephant", 1758);
    Animal animal2("Giant Panda", 1869);
+   AnimalsInZoo an3(animal2);
 
    delete animal1;
    animal1 = new Animal("Snow Leopard", 1777);
 
    animal2.display();
    animal1->display();
+   an3.display();
 
    delete animal1;
 }
